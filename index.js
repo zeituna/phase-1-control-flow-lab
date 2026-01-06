e14a9732669eaaca760126cafac470bb8654f3ae
leila <laila.ahmed.2645@gmail.com>
2026-01-06 13:57:48 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+// 1. scuberGreetingForFeet()
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return "This one is on me!";
+  } else if (someValue > 2000 && someValue <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (someValue > 2500) {
+    return "No can do.";
+  } else {
+    return "That will be twenty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// 2. ternaryCheckCity()
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// 3. switchOnCharmFromTip()
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
+
+// Export functions for testing
+module.exports = {
+  scuberGreetingForFeet,
+  ternaryCheckCity,
+  switchOnCharmFromTip
+};
+
+
